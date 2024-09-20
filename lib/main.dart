efedd9fd73fa1d074d03b23564efcdeb11177359
LeoJPGM <leojp4781@gmail.com>
2024-09-20 18:06:26 -0500

    Inicio del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:delivery_app/src/login/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Delivery App Flutter",
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login',
+      routes: {'login': (BuildContext context) => const LoginPage()},
+    );
+  }
+}
